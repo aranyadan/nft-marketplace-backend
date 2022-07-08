4d87e41ec66072d9ac56d8fb0d231e82981c6046
Aranya Dan <aranya.dan1996@gmail.com>
2022-07-09 01:31:02 +0200

    Added mint script

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,9 +4,7 @@ const { moveBlocks } = require("../utils/move-blocks")
 async function mintAndList() {
     const accounts = await ethers.getSigners()
     const deployer = accounts[0]
-    const nftMarketplace = await ethers.getContract("NftMarketplace", deployer)
     const basicNft = await ethers.getContract("BasicNFT", deployer)
-    const PRICE = ethers.utils.parseEther("0.1")
 
     // Mint NFT
     console.log("Minting...")
@@ -14,15 +12,8 @@ async function mintAndList() {
     const mintTxReceipt = await tx.wait(1)
     const tokenId = mintTxReceipt.events[0].args.tokenId
     console.log("Minted!")
-
-    // List Item
-    console.log("Approving...")
-    tx = await basicNft.approve(nftMarketplace.address, tokenId)
-    await tx.wait(1)
-    console.log("Listing Nft...")
-    tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
-    await tx.wait(1)
-    console.log("Listed!")
+    console.log(`Got TokenID: ${tokenId}`)
+    console.log(`NftAddress: ${basicNft.address}`)
 
     if (network.config.chainId == "31337") {
         await moveBlocks(2, 1000)
